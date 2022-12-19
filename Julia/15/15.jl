eba676f4be790d83b0f78269eda529f285a8f1ae
elizavetaselikhanova <120097437+elizavetaselikhanova@users.noreply.github.com>
2022-12-19 15:09:33 +0300

    Update 15.jl

--- Julia/15/15.jl
+++ Julia/15/15.jl
@@ -5,7 +5,7 @@
 #using HorizonSideRobots
 #include("15.jl")
 #r=Robot("15.sit",animate=true)
-#solve!(r)
+#cross!(r)
 
 function cross!(robot) #Функция, ставящая крест
     putmarker!(robot)
@@ -16,20 +16,19 @@ end
 
 
 function diagonal_along!(robot, side1, side2) #Робот идет по диагонали и ставет маркеры
-  x=y=0
-  while try_move_along!(robot,side1,side2)
+  x=0 #Количество шагов домой
+  while try_move_along!(robot,side1,side2) #робот идет и ставит маркеры
     x+=1
   end
-  for i in 1:x
-    move_recursion!(robot,inverse(side1))
+  for i in 1:x #Идет домой
+    move_recursion!(robot,inverse(side1)) 
     move_recursion!(robot,inverse(side2))
   end
 end
 
 
-function try_move_along!(robot,side1,side2) #Диагональный along 
-    if (try_move!(robot,side1))
-        if (try_move!(robot,side2))
+function try_move_along!(robot,side1,side2) #Диагональный along. Сначала идет в сайд 1 и сайд 2, если не получилось идет в предыдущую клетку и сайд 2 сайд 1  if (try_move!(robot,side1))
+        if (try_move!(robot,side2))         #и если не получилось то домой. И после каждого удачного прохода по диагонали ставит маркер
             putmarker!(robot)
             return true
         else
@@ -50,19 +49,7 @@ function try_move_along!(robot,side1,side2) #Диагональный along
        
 end
 
-
-function move_recursion!(robot,side) #Рекурсивный способ обхода стены
-    if (isborder(robot,side))
-        move!(robot,right(side))
-        move_recursion!(robot,side)
-        move!(robot,inverse(right(side)))
-    else
-        move!(robot,side)
-    end
-end
-
-
-function try_move!(robot,side)
+function try_move!(robot,side) #Если есть перегородка не идем, если нет перегородки, то идем
      if (!isborder(robot,side))
         move!(robot,side)
         return true
@@ -72,9 +59,6 @@ function try_move!(robot,side)
 end
 
 
-function solve!(robot)
-   cross!(robot)
-end
 
 
 inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)
